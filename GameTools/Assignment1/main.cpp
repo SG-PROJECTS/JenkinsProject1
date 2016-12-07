f7a1cd36a7078d0054ff6efa23bbb8f3df580164
shiva14 <gupta.shiva14@gmail.com>
2016-12-06 21:54:44 -0800

    Changed output to only binary

--- GameTools/Assignment1/main.cpp
+++ GameTools/Assignment1/main.cpp
@@ -17,11 +17,10 @@ void main(int argc, char *argv[])
 	FILE* inputFile = nullptr;
 	FILE* outputFile = nullptr;
 	errno_t error = fopen_s(&inputFile, argv[1], "r");
-	errno_t error2 = fopen_s(&outputFile, argv[2] , "w");
 
 	float delta = (float)atof(argv[3]);
 
-	if (error != 0 && error2!= 0)
+	if (error != 0)
 	{
 		return;
 	}
@@ -46,10 +45,8 @@ void main(int argc, char *argv[])
 	buffer.append(" ");
 	buffer.append("\n");
 
-	fprintf_s(outputFile, "size: %d\n", nodes.size());
 	for(auto& node : nodes)
-	{
-		fprintf_s(outputFile, "%f %f %f\n", node.x, node.y, node.z);
+	{		
 		buffer.append(std::to_string(node.x));
 		buffer.append(" ");
 		buffer.append(std::to_string(node.y));
@@ -61,11 +58,9 @@ void main(int argc, char *argv[])
 	
 	std::string data;
 	unsigned long sizeDataCompressed = 0;
-	ZLib::Compress(buffer, "savedData.dat", sizeDataCompressed);
-	ZLib::UnCompress(data, "savedData.dat", buffer.size(), sizeDataCompressed);
+	ZLib::Compress(buffer, argv[2], sizeDataCompressed);
 
 	fclose(inputFile);
-	fclose(outputFile);
 }
 
 // TODO Test for constructor for Vector Class, distance formula, removing points formula. Also show a failed test!! 
